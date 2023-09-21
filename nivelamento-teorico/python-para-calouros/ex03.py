4c4d9e4db7fc62f22c6c5346b5b3c9a5f1d8d9fa
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-09-21 11:38:25 -0300

    att ex03

--- nivelamento-teorico/python-para-calouros/ex03.py
+++ nivelamento-teorico/python-para-calouros/ex03.py
@@ -1,1 +1,9 @@
 # escreva um programa que leia duas notas de um aluno de programação. Em seguida, a média ponderada, com pesos 2 e 3, respectivamente, deve ser calculada. Por fim, O programa deve imprimir a média obtida
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+
+mediaPonderada = ((nota1 * 2) + (nota2 * 3)) / (2 + 3)
+
+print(f'Sua nota final é {mediaPonderada:.2f}')
+
